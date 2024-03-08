0913bcbc612f7e8a1a1e704898de79637723e652
yaphet91 <157224747+yaphet91@users.noreply.github.com>
2024-03-08 23:18:43 +0300

    Update countIndex.js
    updated date

--- scripts/countIndex.js
+++ scripts/countIndex.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Mar 8, 2024',
+  birthdate: 'Mar 9, 2024',
   name: 'Tiobiesta'
 };
 
